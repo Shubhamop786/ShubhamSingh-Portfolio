9f93bc7c2934f982c0a68ecf97d00ac3d4cc93af
Shubham Singh <singhshubham9283@gmail.com.com>
2024-09-07 17:56:01 +0530

    Update Header component styles and logic

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,26 +1,37 @@
 // src/components/Header.js
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../assets/images/shubhamlogo3.webp';
 import './Header.css';
 
 function Header() {
-    return (
-      <header className="header">
-        <div className="logo">
-          <a href="#home"><img src={logo} alt="Logo" /></a>
-        </div>
-        <nav>
-          <ul>
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#skill">Skills</a></li>
-            <li><a href="#certificates">Certificates</a></li>
-            <li><a href="#projects">Projects</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </nav>
-      </header>
-    );
-  }
-  
-  export default Header;
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <header className="header">
+      <div className="logo">
+        <a href="#home"><img src={logo} alt="Logo" /></a>
+      </div>
+      <button className="burger-menu" onClick={toggleMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </button>
+      <nav className={`nav ${isMenuOpen ? 'open' : ''}`}>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#skill">Skills</a></li>
+          <li><a href="#certificates">Certificates</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
